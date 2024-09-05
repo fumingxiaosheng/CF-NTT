6c4bba8f72679ffec176a45a31a60931da807f8b
fumingxiaosheng <xiaowenhu_hust@163.com>
2024-09-05 16:04:25 +0800

    8989

--- test.cu
+++ test.cu
@@ -1,4 +1,4 @@
 __global__ void test32(){
     printf("%d, %d, %d %d %d\n",blockDim.x,blockDim.y,blockDim.z,threadIdx.x,threadIdx.y);
-    cout << "this is huxiaowe  " <<endl;
+    cout << "this is huxiaowen8989  " <<endl;
 }
