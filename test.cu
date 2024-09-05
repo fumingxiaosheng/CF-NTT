8fca6d689056f29ee6095616cd72741b919f8f2a
fumingxiaosheng <xiaowenhu_hust@163.com>
2024-09-05 16:02:38 +0800

    modify test v3

--- test.cu
+++ test.cu
@@ -1,4 +1,4 @@
 __global__ void test32(){
     printf("%d, %d, %d %d %d\n",blockDim.x,blockDim.y,blockDim.z,threadIdx.x,threadIdx.y);
-    cout << "this is huxiaowen v2" <<endl;
+    cout << "this is huxiaowe  " <<endl;
 }
